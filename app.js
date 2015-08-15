abb1b8e66e154fbc66ca4f034a24e4b711d5eef2
Clarity-89 <homes89@ukr.net>
2015-08-15 21:08:45 +0300

    Got JSON data from the Twitch TV API

--- app.js
+++ app.js
@@ -21,5 +21,44 @@ $(document).ready(function () {
 
     });
 
-    var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","comster404","brunofin","thomasballinger","noobs2ninjas","beohoff"];
+    var users = ["FreeCodeCamp", "GeoffStorbeck", "terakilobyte", "habathcx", "RobotCaleb", "medrybw", "thomasballinger", "noobs2ninjas", "beohoff"];
+
+    var urls = users.map(function (el) {
+        return 'https://api.twitch.tv/kraken/streams/' + el + '?callback=?';
+    });
+    /* var m = [];
+     var aj = urls.forEach(function (url) {
+     $.getJSON(url).done(function (data) {
+     //console.log(data);
+
+     }).then(function (data) {
+     m.push(data.stream);
+     })
+     });
+     $.when(aj).then(function(d){
+     console.log(m);
+     })*/
+
+    var requests = [];
+
+    urls.forEach(function (el) {
+        // Submit the ajax request
+        requests.push($.getJSON(el, function (data) {
+            console.log('req submitted');
+        }))
+    });
+    // After all the data from the request is received, create a handlebars.js template
+    $.when.apply(null, requests).then(function (data) {
+        var source = $("#users").html();
+        var template = Handlebars.compile(source);
+        var channels = requests.map(function (el, i, arr) {
+            return {name: users[i], link: 'http://www.twitch.tv/' + users[i], online: el.responseJSON.stream};
+        });
+        var context = template({
+            channels: channels
+        });
+        $('.list-group').html(context);
+        console.log(context);
+    });
+
 });
